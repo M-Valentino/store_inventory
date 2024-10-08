713d07a37b4c7ec37d65ac7e34e860ebb6c5b425
Mark Valentino <mike9377mike9377@gmail.com>
2024-10-08 17:03:48 -0400

    can fetch data from db on frontend

--- store_inventory/views.py
+++ store_inventory/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.shortcuts import render
+from data.models import Item
 
 def homepage(request):
   return render(request, 'home.html')
@@ -10,31 +11,6 @@ def about(request):
 
 @require_http_methods(["GET"])
 def inventory(request):
-  data = [
-    {
-        "name": "Snickers Full Size Chocolate Candy Bar",
-        "upc": "040000424314",
-        "count": 10
-    },
-    {
-        "name": "Coca-Cola 12 fl oz Cans, 12 Pack",
-        "upc": "049000050103",
-        "count": 15
-    },
-    {
-        "name": "Lay's Classic Potato Chips, 8 oz Bag",
-        "upc": "028400040851",
-        "count": 20
-    },
-    {
-        "name": "Campbell's Chicken Noodle Soup, 10.75 oz Can",
-        "upc": "051000012515",
-        "count": 25
-    },
-    {
-        "name": "Tropicana Orange Juice, 52 fl oz",
-        "upc": "048500120124",
-        "count": 12
-    },
-    ]
-  return JsonResponse(data, safe=False)
+    items = Item.objects.all().values("name", "upc", "qty")
+    items_list = list(items)
+    return JsonResponse(items_list, safe=False)
