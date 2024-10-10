28209b079921114147e5c8997bfd088c2262e77e
Mark Valentino <mike9377mike9377@gmail.com>
2024-10-10 01:03:11 -0400

    polished up basic info

--- static/js/productDisplay.js
+++ static/js/productDisplay.js
@@ -3,7 +3,7 @@ const openProductDetails = (name, category, upc, qty) => {
   document.getElementById("product-name-h1").innerHTML = name;
   document.getElementById('categoryChange').value = category;
   document.getElementById('upcUpdate').value = upc;
-  document.getElementById('qtyUpdate').value = qty;
+  document.getElementById('qtyUpdate').innerHTML = qty;
 };
 
 const closeProductDetails = () => {
