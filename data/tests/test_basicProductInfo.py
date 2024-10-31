258c4e366ba2ddbb74270ad987b62b6b2f68ff83
Mark Valentino <mike9377mike9377@gmail.com>
2024-10-31 04:11:55 -0400

    added another test

--- data/tests/test_basicProductInfo.py
+++ data/tests/test_basicProductInfo.py
@@ -10,7 +10,7 @@ class BasicProductInfoViewTest(TestCase):
         Item.objects.create(name='Salmon Filets', category='Seafood', upc='123456789013', qty=5)
         Item.objects.create(name='Oranges', category='Produce', upc='123456789014', qty=20)
 
-    def test_update_upc(self):
+    def test_update_valid_new_and_old_upc(self):
         url = reverse('basicProductInfo')
         response = self.client.post(f"{url}?oldUpc=123456789012&newUpc=223456789012")
         self.assertEqual(response.status_code, 200)
@@ -23,5 +23,12 @@ class BasicProductInfoViewTest(TestCase):
                 "qty": 10
             }
         }
-        self.assertEqual(response.json(), expected_response)
+        self.assertEqual(response.json(), expected_response, 'A product with an existing UPC can be updated to a new UPC.')
+
+    def test_update_valid_new_invalid_old_upc(self):
+        url = reverse('basicProductInfo')
+        response = self.client.post(f"{url}?oldUpc=123426789013&newUpc=223459789012")
+        self.assertEqual(response.status_code, 403)
+        expected_response = {"message": "Product with the provided old UPC does not exist."}
+        self.assertEqual(response.json(), expected_response, 'A saved UPC that does not exist cannot be updated to a new UPC.')
         
