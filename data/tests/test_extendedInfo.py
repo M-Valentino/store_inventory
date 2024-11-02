9af3037aad3ebed76f7bf2c674b958c15a5358c6
Mark Valentino <mike9377mike9377@gmail.com>
2024-11-02 19:19:20 -0400

    added description update test

--- data/tests/test_extendedInfo.py
+++ data/tests/test_extendedInfo.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 from data.models import Item
+import base64
+from urllib.parse import quote
 
 class ExtendedInfoViewTest(TestCase):
 
@@ -16,4 +18,15 @@ class ExtendedInfoViewTest(TestCase):
         expected_response = {"message": 'These are sweet apples.', "id": 1}
         self.assertEqual(response.json(), expected_response, 'An existing product should have its description returned.')
 
-    
+    def test_update_description(self):
+        url = reverse('extendedInfo')
+        description = 'Lorem ipsum. test test'
+        encoded_description = base64.b64encode(quote(description).encode('utf-8')).decode('utf-8')
+        
+        response = self.client.post(
+            url, 
+            data={'upc': '123456789013', 'description': encoded_description}
+        )
+        self.assertEqual(response.status_code, 200)
+        expected_response = {"message": "success", "new_description": "Lorem ipsum. test test"}
+        self.assertEqual(response.json(), expected_response, 'A product with an existing UPC can be updated with a new description.')
